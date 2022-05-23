4a02844ecea55c2f8d65dcf054340fa3e647c275
Luis Lopez <lglvluislopez@gmail.com>
2022-05-22 19:21:49 -0500

    rework needed

--- src/lr/LogisticRegressionFormulas.java
+++ src/lr/LogisticRegressionFormulas.java
@@ -41,21 +41,36 @@ public class LogisticRegressionFormulas {
         return learningRate;
     }
 
+    public void reasignWeights(double w0, double w1, double w2) {
+        this.w0 = w0;
+        this.w1 = w1;
+        this.w2 = w2;
+    }
+
+    public double getSigmoid() {
+        // sigmoid function
+        // get the correct value from the training set
+        double x = this.w0 + (this.w1 * this.trainingSet[0][0]) + (this.w2 * this.trainingSet[0][1]);
+        double sigmoid = 1 / (1 + Math.exp(-x));
+        return sigmoid;
+    }
+
     public void calculateGradientDescent(double w, int j) {
         double sum = 0;
         for(int i = 0; i < trainingSet.length; i++) {
-            sum += (trainingSet[i][3] - 0.5) * trainingSet[i][j];
+            sum += (trainingSet[i][3] - getSigmoid() ) * trainingSet[i][j];
         }
         wArray[j] = w - (learningRate * sum);
         System.out.println("w" + j + ": " + wArray[j]);
     }
 
     public void generateModel() {
-        // for(int i =0; i<100; i++) {
+        for(int i =0; i<100; i++) {
             calculateGradientDescent(wArray[0], 0);
             calculateGradientDescent(wArray[1], 1);
             calculateGradientDescent(wArray[2], 2);
-        // }
+            reasignWeights(wArray[0], wArray[1], wArray[2]);
+        }
     }
 
 }
